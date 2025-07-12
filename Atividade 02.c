b8cac053fa9a65054dfc9c22ce9d102b33681458
João Marcelo <marcelojoao999543293@gmail.com>
2025-07-12 09:11:21 -0300

    Atividade 02.c

---
+++ Atividade 02.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int main(){
+ printf("********** \n");
+ printf("* * \n");
+ printf("* * \n");
+printf("* * \n");
+printf("* * \n");
+printf("* * \n");
+printf("********** \n");
+printf(" \n");
+rintf(" *** \n");
+printf("* * \n");
+ printf("* * \n");
+printf("* * \n");
+ printf("* printf("**6)
+printf(" ***);
+printf(" \n );
+printf(" \n");
+printf("  *  \n");
+printf(" *** \n");
+ printf(" ***** \n");
+ printf("******* \n");
+ printf("********* \n");
+printf(" * \n");
+ printf(" \n");
+printf(" \n");
+printf(" \n");
+ printf(" ** \n");
+ printf("* * \n");
+ rintf("* * \n")1
+ printf(" * * \n");
+ printf(" ** \n");
+ printf(" * \n");
+ printf("\n"); 
+
+return 0;
+ }
+
+#include <stdio.h>
+int main (){
+ float fahrenheit;
+ printf("Informe o valor de fahrenheit: ");
+ scanf("%s", fahrenheit);
+float celsius =((fahrenheit 32) * 5)/9;
+
+printf("---Transformação da temperatura");
+
+printf("Informe a nova temperatura em fahrenheit:%f", celsius);
+}
+
+
+#include <stdio.h>
+int main(){
+
+const float PI= 3.14159;
+
+
+float raio;
+
+float altura;
+
+printf("Informe o valor do raio:");
+scanf("%s",raio);
+printf("Informe o valor da altura:");
+
+scanf("%s", altura);
+
+float volume = PI * raio * altura;
+
+printf("Informe o volume:", volume:
+
+  }
+
+
