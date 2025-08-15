bc9700c1c31a519b8e16f958195e6d975f7606a6
João Marcelo <marcelojoao999543293@gmail.com>
2025-08-15 07:22:29 -0300

    Montante.c

---
+++ Montante.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ int anos;
+ long montante;
+ short num_con;
+
+int main(){
+
+printf("informe o numero da conta: ");
+scanf("%hd",&num_con);
+printf("informe o montante: ");
+scanf("%ld",&montante);
+printf("informe sua idade: ");
+scanf("%d",&anos);
+
+if(mon<=5000){
+printf("o deposito foi realizado!");    
+}else{
+    printf("ultrapassou o limite");
+}  
+}
